537caaf97200b0b0ca071835822256dc26892419
yashsalvi <47267252+yashsalvi@users.noreply.github.com>
2021-04-02 18:53:12 +0530

    Updated it with the lazyLoading feature

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
+
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
   {
@@ -19,7 +20,8 @@ const appRoutes: Routes = [
       import('./auth/auth.module').then((m) => m.AuthModule),
   },
 ];
-
+    
+    
 @NgModule({
   imports: [RouterModule.forRoot(appRoutes,{ preloadingStrategy: PreloadAllModules})
 ],
