3f7f7535c67547f3b98ad88432b8741283107ec2
yashsalvi <47267252+yashsalvi@users.noreply.github.com>
2021-04-03 19:16:31 +0530

    Removed the redundant comments

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -14,8 +14,3 @@ export class DropdownDirective{
         this.isOpen = !this.isOpen;
     }
 }
-// @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
-//     this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
-//   }
-//   constructor(private elRef: ElementRef) {}
-// }
