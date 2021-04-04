3fac08100916855be674a9ad5a32b0ed2989322b
yashsalvi <47267252+yashsalvi@users.noreply.github.com>
2021-04-04 17:50:37 +0530

    Update recipes-resolver.service.ts

--- src/app/recipes/recipes-resolver.service.ts
+++ src/app/recipes/recipes-resolver.service.ts
@@ -8,8 +8,6 @@ import { RecipeService } from "./recipe.service";
 export class RecipeResolverService implements Resolve<Recipe[]>{
     constructor(private dataStorageService:DataStorageService,
         private recipeService:RecipeService){}
-
-
 resolve(route:ActivatedRouteSnapshot,state:RouterStateSnapshot){
     const recipes=this.recipeService.getRecipes();
     if(recipes.length === 0){
