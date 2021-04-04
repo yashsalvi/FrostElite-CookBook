5486914e9aa761725f8efe972fce28129404df16
yashsalvi <47267252+yashsalvi@users.noreply.github.com>
2021-04-04 17:30:13 +0530

    Configured the Environment variable

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseAPIKey:'AIzaSyDqCeUoW4mrqsXQQ3GUNsq5CTuub1t7m-g'
 };
 
 /*
