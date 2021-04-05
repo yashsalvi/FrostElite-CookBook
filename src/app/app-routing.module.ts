ca4f5eff851c085d7ad44823dd4970569e0d596b
yashsalvi <47267252+yashsalvi@users.noreply.github.com>
2021-04-05 20:45:40 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
-
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
   {
@@ -20,13 +19,13 @@ const appRoutes: Routes = [
       import('./auth/auth.module').then((m) => m.AuthModule),
   },
 ];
-    
-    
+     
 @NgModule({
   imports: [RouterModule.forRoot(appRoutes,{ preloadingStrategy: PreloadAllModules })
 ],
   exports: [RouterModule],
 })
+
 export class AppRoutingModule {}
 
 
