a8735c14d143604d2847ad16e8c77d22d2682d2b
yashsalvi <47267252+yashsalvi@users.noreply.github.com>
2021-04-05 20:48:09 +0530

    Update auth-interceptor.service.ts

--- src/app/auth/auth-interceptor.service.ts
+++ src/app/auth/auth-interceptor.service.ts
@@ -5,6 +5,7 @@ import { AuthService } from "./auth.service";
 
 
 @Injectable()
+
 export class AuthInterceptorService implements HttpInterceptor{
     constructor(private authService:AuthService){}
     intercept(req:HttpRequest<any>,next:HttpHandler){
