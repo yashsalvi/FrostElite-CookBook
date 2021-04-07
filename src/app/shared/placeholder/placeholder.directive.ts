73a70148e046e2374c1e728ff221d25097b624a9
yashsalvi <47267252+yashsalvi@users.noreply.github.com>
2021-04-07 19:49:24 +0530

    Update placeholder.directive.ts

--- src/app/shared/placeholder/placeholder.directive.ts
+++ src/app/shared/placeholder/placeholder.directive.ts
@@ -3,7 +3,6 @@ import { Directive, ViewContainerRef } from "@angular/core";
 @Directive({
 selector:'[appPlaceholder]'
 })
-
 export class PlaceholderDirective{
     constructor(public viewContainerRef: ViewContainerRef){}
 }
