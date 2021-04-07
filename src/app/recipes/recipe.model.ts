b7ec88f9e9925fa5e987215284066dcec5e212bb
yashsalvi <47267252+yashsalvi@users.noreply.github.com>
2021-04-07 19:48:45 +0530

    Update recipe.model.ts

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -5,7 +5,8 @@ export class Recipe {
     public description: string;
     public imagePath: string;
     public ingredients: Ingredient[];
-
+  
+  
 constructor(name:string,desc:string,imagePath:string,ingredients:Ingredient[]){
     this.name=name;
     this.description=desc;
