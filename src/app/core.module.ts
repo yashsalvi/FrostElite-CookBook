dafb3a670fac403f0dc31783c4342d67653140a2
yashsalvi <47267252+yashsalvi@users.noreply.github.com>
2021-04-08 18:57:00 +0530

    Update core.module.ts

--- src/app/core.module.ts
+++ src/app/core.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from "@angular/core";
 import { HTTP_INTERCEPTORS } from "@angular/common/http";
-
 import { AuthInterceptorService } from "./auth/auth-interceptor.service";
 import { RecipeService } from "./recipes/recipe.service";
 import { ShoppingListService } from "./shopping-list/shopping-list.service";
