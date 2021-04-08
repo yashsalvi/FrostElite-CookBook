52486d785fe3dc90e78704acfafef7a210dcebaf
yashsalvi <47267252+yashsalvi@users.noreply.github.com>
2021-04-08 18:54:53 +0530

    Update auth.module.ts

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,8 +1,7 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
-import { RouterModule } from "@angular/router";
-        
+import { RouterModule } from "@angular/router";     
 import { AuthComponent } from "./auth.component";
 import { SharedModule } from "../shared/shared.module";
 
