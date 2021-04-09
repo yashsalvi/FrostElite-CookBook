37f0fa1db2cdf3b9d8933d6114a71564be7f0c74
yashsalvi <47267252+yashsalvi@users.noreply.github.com>
2021-04-09 18:24:34 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,7 @@ import { AuthService } from './auth/auth.service';
 })
 export class AppComponent implements OnInit {
   constructor(private authService:AuthService){}
-
+  
   ngOnInit(){
-    this.authService.autoLogin()
-  }
+    this.authService.autoLogin() }
 }
