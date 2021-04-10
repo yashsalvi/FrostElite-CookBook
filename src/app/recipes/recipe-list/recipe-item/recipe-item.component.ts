67141e6cf220c1de337d7a180928546c015adf64
yashsalvi <47267252+yashsalvi@users.noreply.github.com>
2021-04-10 16:06:15 +0530

    Update recipe-item.component.ts

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit,Input } from '@angular/core';
 
 import { Recipe } from '../../recipe.model';
 
-
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
