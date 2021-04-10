5ef4015c978380ab705c0d0e9eb34db1af253fb1
yashsalvi <47267252+yashsalvi@users.noreply.github.com>
2021-04-10 16:05:45 +0530

    Update recipe-list.component.ts

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -3,13 +3,13 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import{Recipe} from '../recipe.model';
 import{RecipeService} from '../recipe.service';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit,OnDestroy {
- 
   recipes:Recipe[];
   subscription:Subscription
  
@@ -28,7 +28,7 @@ export class RecipeListComponent implements OnInit,OnDestroy {
   }
 
   ngOnDestroy(){
-    this.subscription.unsubscribe()               // To make sure we don't cause any memory leaks here.
+    this.subscription.unsubscribe()              
   }
   
 
