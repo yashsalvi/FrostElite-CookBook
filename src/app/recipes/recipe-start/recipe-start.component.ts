095f33a5204b87e7ef2ea6ceb0fdae64e2a18e6e
yashsalvi <47267252+yashsalvi@users.noreply.github.com>
2021-04-10 16:07:47 +0530

    Update recipe-start.component.ts

--- src/app/recipes/recipe-start/recipe-start.component.ts
+++ src/app/recipes/recipe-start/recipe-start.component.ts
@@ -6,7 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-start.component.css']
 })
 export class RecipeStartComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit(): void {
