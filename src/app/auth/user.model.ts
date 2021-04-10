8b3c8ff755295747e2ab17f9b8a70e74c503adc7
yashsalvi <47267252+yashsalvi@users.noreply.github.com>
2021-04-10 16:03:06 +0530

    Update user.model.ts

--- src/app/auth/user.model.ts
+++ src/app/auth/user.model.ts
@@ -5,6 +5,7 @@ export class User{
         private _token:string,
         private _tokenExpirationDate:Date
     ){}
+  
 get token(){
     if(!this._tokenExpirationDate || new Date() > this._tokenExpirationDate){
         return null;
