0ea39ffd808f84a297eb5c48eaf04d1a707d88be
yashsalvi <47267252+yashsalvi@users.noreply.github.com>
2021-04-12 17:17:55 +0530

    Update auth.guard.ts

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -5,8 +5,6 @@ import { Observable } from "rxjs";
 import { map, take } from "rxjs/operators";
 import { AuthService } from "./auth.service";
 
-
-
 @Injectable({providedIn:'root'})
 export class AuthGuard implements CanActivate{
     constructor(private authService:AuthService,
