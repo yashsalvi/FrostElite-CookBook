57a2d5f289e6588d30d63970e07c880871869ad8
yashsalvi <47267252+yashsalvi@users.noreply.github.com>
2021-04-13 19:06:38 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from './auth/auth.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
